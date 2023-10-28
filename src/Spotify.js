007c2eca66db4b3b6c1675c6db43a2fe7603ef94
mintygrayyy <northpole15@gmail.com>
2023-10-28 16:37:53 -0400

    Added login features

--- src/Spotify.js
+++ src/Spotify.js
@@ -4,7 +4,9 @@ const CLIENT_ID = "f0fa87358a7141a2bac1091e55d80a04"
 const REDIRECT_URI = "http://localhost:3000"
 const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
 const RESPONSE_TYPE = "token"
+const SPACE = "%20"
 const SCOPES = ["playlist-read-private", "playlist-read-collaborative"]
+const SCOPES_URL = SCOPES.join(SPACE)
 const Spotify = () => {
 
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,24 @@
-import { React, useState } from 'react'
+import { React, useState, useEffect } from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+//import '../Spotify'
 import { Container, Row, Col, Button } from 'react-bootstrap'
 
-const Login = () => {
-    const [logged, setLogged] = useState(false) // Has the user logged in?
-    const [inputfields, setInputFields] = useState(false);
+const CLIENT_ID = "f0fa87358a7141a2bac1091e55d80a04"
+const REDIRECT_URI = "http://localhost:3000/rewrapped"
+const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+const RESPONSE_TYPE = "token"
+const SPACE = "%20"
+const SCOPES = ["playlist-read-private", "playlist-read-collaborative"];
+const SCOPES_URL = SCOPES.join(SPACE)
 
+const Login = () => {
+    //const [logged, setLogged] = useState(false) // Has the user logged in?
+    //const [inputfields, setInputFields] = useState(false);
+    
+    const handleLogin = () => {
+        window.location = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPES_URL}&response_type=token&show_dialog=true`;
+    };
 
     return (
         <div>
@@ -14,7 +26,7 @@ const Login = () => {
             <Container>
                 <Row>
                     <Col sm={8} md={8} lg={8}>
-                        <Button>Log into Spotify</Button>
+                        <button onClick = {handleLogin}>Log into Spotify</button>
                     </Col>
                 </Row>
             </Container>
