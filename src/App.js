360a51bcb61b7d38c4e67786f1108d87cab432e0
mintygrayyy <northpole15@gmail.com>
2023-10-29 05:23:18 -0400

    Display playlists :D

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
+import GetPlaylist from './pages/SpotifyGetPlaylists';
 import Login from './pages/Login'
+import SpotifyGetPlaylists from './pages/SpotifyGetPlaylists';
 
 const CLIENT_ID = "f0fa87358a7141a2bac1091e55d80a04";
 const CLIENT_SECRET = "";
@@ -8,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Login />
+      <SpotifyGetPlaylists />
     </div>
   );
 }
